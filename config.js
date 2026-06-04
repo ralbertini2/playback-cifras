2e4bca7103a02c9d4a4e4413883e5441ec557a17
ralbertini2 <raphael.albertini@gmail.com>
2026-06-04 20:51:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // 2) Ative a Google Drive API.
 // 3) Coloque seu domínio do GitHub Pages como origem autorizada.
 window.APP_CONFIG = {
-  GOOGLE_CLIENT_ID: "COLE_SEU_CLIENT_ID_AQUI.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "1012476481731-4if068gl92lg6uhm840ms872lludtlii.apps.googleusercontent.com",
   ROOT_FOLDER_ID: "" // opcional: ID da pasta principal do Drive
 };
