0684edf318f7801081688caa986ac4f979b605da
ralbertini2 <raphael.albertini@gmail.com>
2026-06-04 22:09:26 -0300

    Update config.js
    API Key v2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   GOOGLE_CLIENT_ID: "1012476481731-4if068gl92lg6uhm840ms872lludtlii.apps.googleusercontent.com",
-  GOOGLE_API_KEY: "AIzaSyC_QWmYDWKM7soUMJQfndCIYZFoB8B-kNE",
+  GOOGLE_API_KEY: "AIzaSyCuKmzGI1J5JcaUMsqr5UgXqYbEvmHeeGM",
   ROOT_FOLDER_ID: ""
 };
