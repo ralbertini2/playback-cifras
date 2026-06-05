215b38d4db28e8ba85a93f118873720b96a0c8d5
ralbertini2 <raphael.albertini@gmail.com>
2026-06-04 21:29:34 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   GOOGLE_CLIENT_ID: "1012476481731-4if068gl92lg6uhm840ms872lludtlii.apps.googleusercontent.com",
-  GOOGLE_API_KEY: "", // opcional, necessário apenas para o Google Picker funcionar em alguns navegadores
+  GOOGLE_API_KEY: "AIzaSyC_QWmYDWKM7soUMJQfndCIYZFoB8B-kNE",
   ROOT_FOLDER_ID: ""
 };
